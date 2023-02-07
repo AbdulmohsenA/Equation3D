033d2c7c81f6c2197331c64391d86d4022e8d436
Abdulmohsen <a.abanmy113@gmail.com>
2023-02-07 18:29:47 +0300

    Prettifying after linter's comments

--- js/translate.js
+++ js/translate.js
@@ -1,16 +1,16 @@
 export function translate (expression) {
-    let trig = {
-        "cos" : "Math.cos",
-        "sin" : "Math.sin",
-        "tan" : "Math.tan"
+    const trig = {
+      'cos': 'Math.cos',
+      'sin': 'Math.sin',
+      'tan': 'Math.tan'
     }
-
+  
     for (const [key, value] of Object.entries(trig)) {
-        expression = expression.replace(key, value)
-      }
-    
+      expression = expression.replace(key, value)
+    }
+  
     // For now, we only have 2 inputs for any function.
     // This way of evaluating was done to increase performance.
-    let outputFunction = eval("(x, t) => {return " + expression + "}")
+    const outputFunction = eval('(x, t) => {return ' + expression + '}')
     return outputFunction
-}
+  }
--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 import '/style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as dat from 'dat.gui'
 import { translate } from './js/translate'
 
 // Container
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 // camera(FOV, Aspect Ratio, view start, view end)
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg")
+  canvas: document.querySelector('#bg')
 })
 
 scene.background = new THREE.Color(0x000000)
 
-renderer.setPixelRatio( window.devicePixelRatio )
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(150)
 
@@ -24,30 +24,27 @@ const controls = new OrbitControls(camera, renderer.domElement)
 
 //#region Wave surface
 
-let planeGeometry = new THREE.PlaneGeometry(500, 500, 150, 150)
+const planeGeometry = new THREE.PlaneGeometry(500, 500, 150, 150)
 
-let pointColor = new THREE.Color(0xffffff)
+const pointColor = new THREE.Color(0xffffff)
 
-let pointMaterial = new THREE.PointsMaterial( {
+const pointMaterial = new THREE.PointsMaterial({
   size: 0.1,
   color: pointColor
-} )
+})
 
-let plane = new THREE.Points(planeGeometry, pointMaterial)
+const plane = new THREE.Points(planeGeometry, pointMaterial)
 
 plane.position.set(0, -100, -200)
-plane.rotateX(3*Math.PI/2)
-plane.rotateZ(Math.PI/2)
+plane.rotateX(3 * Math.PI / 2)
+plane.rotateZ(Math.PI / 2)
 const count = planeGeometry.attributes.position.count
 const clock = new THREE.Clock()
 
 scene.add(plane)
 
-let MAXHEIGHT = 500
+const MAXHEIGHT = 500
 //#endregion
-
-
-
 
 const gui = new dat.GUI()
 
@@ -56,70 +53,64 @@ const controlValues = {
   speed: 1,
   backgroundColor: 0x000000,
   pointClr: 0xffffff,
-  expression: "cos(x * 20 + t)"
+  expression: 'cos(x * 20 + t)'
 }
 
 let expression = controlValues.expression
 let mathFunction = translate(expression)
 
 //#region GUI
-//DEBUGGER
+// DEBUGGER
 
-gui.add(controlValues, "expression").onFinishChange((exp) => {
+gui.add(controlValues, 'expression').onFinishChange((exp) => {
   mathFunction = translate(exp)
-}).name("Expression");
+}).name('Expression')
 
+const controlsGUI = gui.addFolder('Controls')
 
-let controlsGUI = gui.addFolder("Controls")
+controlsGUI.add(controlValues, 'scale', 5, 250, 5).name('Scale')
+controlsGUI.add(controlValues, 'speed', 1, 50, 0.1).name('Speed')
 
-controlsGUI.add(controlValues, "scale", 5, 250, 5).name("Scale")
-controlsGUI.add(controlValues, "speed", 1, 50, 0.1).name("Speed")
+const rotationGUI = gui.addFolder('Rotation')
 
-let rotationGUI = gui.addFolder("Rotation")
+rotationGUI.add(plane.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Axis')
+rotationGUI.add(plane.rotation, 'y', 0, Math.PI * 2, 0.01).name('Y Axis')
+rotationGUI.add(plane.rotation, 'z', 0, Math.PI * 2, 0.01).name('Z Axis')
 
-rotationGUI.add(plane.rotation, "x", 0, Math.PI * 2, 0.01).name('X Axis')
-rotationGUI.add(plane.rotation, "y", 0, Math.PI * 2, 0.01).name('Y Axis')
-rotationGUI.add(plane.rotation, "z", 0, Math.PI * 2, 0.01).name('Z Axis')
+const colorsGUI = gui.addFolder('Colors')
 
-let colorsGUI = gui.addFolder("Colors")
-
-colorsGUI.addColor(controlValues, "backgroundColor").listen().onChange((e) => {
+colorsGUI.addColor(controlValues, 'backgroundColor').listen().onChange((e) => {
   scene.background = new THREE.Color(e)
-}).name("Background Color")
+}).name('Background Color')
 
-colorsGUI.addColor(controlValues, "pointClr").listen().onChange((e) => {
+colorsGUI.addColor(controlValues, 'pointClr').listen().onChange((e) => {
   plane.material.color = new THREE.Color(e)
-}).name("Point Color")
+}).name('Point Color')
 
 //#endregion
-
-
 
 let x, t, z
 
 // Animation Loop
-function animate() {
+function animate () {
   requestAnimationFrame(animate)
 
   t = clock.getElapsedTime()
   t *= controlValues.speed
 
   for (let i = 0; i < count; i++) {
-
     x = i / count
-    
     // Point Placement
     z = mathFunction(x, t)
-    z *= controlValues.scale;
+    z *= controlValues.scale
 
-    if (z > MAXHEIGHT) {z = MAXHEIGHT}
+    if (z > MAXHEIGHT) { z = MAXHEIGHT }
 
     plane.geometry.attributes.position.setZ(i, z)
   }
 
   planeGeometry.computeVertexNormals()
   planeGeometry.attributes.position.needsUpdate = true
-  
 
   controls.update()
   renderer.render(scene, camera)
